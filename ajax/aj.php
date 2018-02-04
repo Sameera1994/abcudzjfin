a561ca961a4ca77b28a73c013df114ba0e7816cd
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-02-05 01:32:08 +0530

    Update aj.php

--- ajax/aj.php
+++ ajax/aj.php
@@ -1,4 +1,5 @@
 <?php
+
 include('../connection.php');
 // Array with names
 $a=$_REQUEST['q'];
@@ -7,14 +8,18 @@ $sql="SELECT * FROM designation WHERE cat=$a";
 $result=mysqli_query($conn, $sql);
 $nu=mysqli_num_rows($result);
 //echo $nu;
+
 if($nu>0){
 //echo "<select name='Desig' id='Desig'class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown' onchange='myfunction1()'>";
 echo "<option>--Select Designation--</option>";
-while($row = mysqli_fetch_assoc($result)) {
-echo "<option value='$row[name]'>$row[name]</option>";
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "<option value='$row[name]'>$row[name]</option>";
+  }
+  
+  //echo "</select>";
 }
-//echo "</select>";
-}else{
-echo "error";
+else{
+  echo "error";
 }
+
 ?>
