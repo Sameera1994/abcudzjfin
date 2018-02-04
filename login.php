acc8c694ff4a684d5e3578cf13d25af44eb4f109
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-02-04 23:57:14 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,12 @@
-
 <html>
 	<head>
-
-		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-
-</head>
+	      <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	</head>
 <body>
   
      <a href ="admin.html">Login as Admin </a> </br></br>
      <a href ="user.html">Login as User </a></br></br>
      <a href ="application.html">Register </a> 
               
-              
-
 </body>
 </html>
