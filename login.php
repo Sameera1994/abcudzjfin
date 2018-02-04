f4faa5482ab6c29e968df775b364fb6f677828f6
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-02-04 23:33:11 +0530

    login.php
    Upload the login file.

---
+++ login.php
@@ -0,0 +1,17 @@
+
+<html>
+	<head>
+
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+</head>
+<body>
+  
+     <a href ="admin.html">Login as Admin </a> </br></br>
+     <a href ="user.html">Login as User </a></br></br>
+     <a href ="application.html">Register </a> 
+              
+              
+
+</body>
+</html>
