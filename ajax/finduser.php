18941f3cf6964aad88430ae8f8ac8a9548692630
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-02-05 01:36:00 +0530

    Update finduser.php

--- ajax/finduser.php
+++ ajax/finduser.php
@@ -2,16 +2,17 @@
 include('../connection.php');
 if(isset($_POST['name']))
 {
-$name=trim($_POST['name']);
-$query2=mysqli_query($conn,"SELECT * FROM user_verify WHERE Name LIKE '%$name%' OR UPF LIKE '%$name%'");
-echo "<ul id='sug'>";
-while($query3=mysqli_fetch_array($query2))
-{
-?>
+	$name=trim($_POST['name']);
+	$query2=mysqli_query($conn,"SELECT * FROM user_verify WHERE Name LIKE '%$name%' OR UPF LIKE '%$name%'");
+	
+	echo "<ul id='sug'>";
+	while($query3=mysqli_fetch_array($query2))
+	{
+		?>
 
-<li onclick='fill("<?php echo $query3['Name']; ?>")'><?php echo $query3['Name']; ?></li>
-<?php
-}
+		<li onclick='fill("<?php echo $query3['Name']; ?>")'><?php echo $query3['Name']; ?></li>
+	<?php
+	}
 }
 ?>
 </ul>
